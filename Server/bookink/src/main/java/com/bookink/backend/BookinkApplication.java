6e607a22fa06ae49a54251a3e9f19db63d6d0735
DuYB4ng <quynhphung11@gmail.com>
2026-05-28 13:45:04 +0700

    Bắt Đầu Dự Án

---
+++ Server/bookink/src/main/java/com/bookink/backend/BookinkApplication.java
@@ -0,0 +1,13 @@
+package com.bookink.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookinkApplication.class, args);
+	}
+
+}
